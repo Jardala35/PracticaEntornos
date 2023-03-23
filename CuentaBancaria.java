862e724edc1017c8e402d1138d35f1e63b274756
Pablo Navarro Practica Git <pablonavarroduro@gmail.com>
2023-03-24 00:46:20 +0100

    commit secundaria 1

--- CuentaBancaria.java
+++ CuentaBancaria.java
@@ -3,7 +3,7 @@ class TestCuentaBancaria
 	public static void main(String args[])
 	{
 		CuentaBancaria cuenta = new CuentaBancaria("1234521","Director Banco",100,0);
-		System.out.println("Saldo = " + cuenta.getSaldo());
+	CuentaBancaria c_secundaria = new CuentaBancaria("0123456789","Nombre Apellidos", 1000,5);  
 		cuenta.reintegro(50);
 		cuenta.ingreso(100);
 		System.out.println("Saldo: " + cuenta.getSaldo());
